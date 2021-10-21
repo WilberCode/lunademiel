0cc2878ee1bc7176ce65e32520f95efc68af759b
wilber <parionahuaraca@gmail.com>
2021-10-21 04:34:51 -0500

    cambios

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php get_template_part( 'templates/partials/document-open' ); ?> 
 <!-- Site header markup goes here --> 
- 
+<?php include_once("googletagmanager.php") ?>
 <?php if(is_front_page()){ ?>
 <?php get_template_part( 'templates/views/header-home' ); ?> 
 <?php } else { ?>
--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -6,10 +6,57 @@
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= wp_get_document_title() ?></title>  
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
-     <meta name="keywords" content="Grupo Tendenze Comunicaciones, Ferias digitales, feria digital mamá, feria digital papá, muestra con tu marca, promoción, Belleza, Deco Vintage, Seguros, Nutrición, Maternidad, Accesorios, Video mamá, Detalles, Postres, Flores, 
-Meat Fest, Carnes, Gourmet, Piel Seca, Experiencia, Vinos, Meditación"> 
+     <meta name="keywords" content="Guía para parejas, Luna de miel, deco, home deco, decoración de interiores, diseño de interiores, diseño de interiores de casas, estilo nórdico, decoración departamento, estilo minimalista, casa rustica moderna, estilo industrial, estilo étnico, Boho chic, estilo mediterráneo moda, vivienda, ropa para mujer, cine, entretenimiento, farándula, eventos, actividades en pareja, cinerama, música en vivo, restaurante, novedades, 
+restaurantes románticos, tendencias de boda, celebridades, catering, bares y café, escapadas, fotógrafos de boda, iglesias, nutrición, bodas, paseos, viajes, excursiones, bares, café, belleza, vacaciones, decoración, iglesias, tecnología, fiestas, tips, consejos, dio del padre, regalos papa, copa de américa, entradas, novedades, cartelera, combo cine, entradas, taquilla, butaca, estrenos, terror, comedia, drama, ciencia ficción, romántica, infantil, combos, estrenos, entradas, diversión, sorteos, concursos, bodas, shows, presentaciones, show musical, concierto, espectáculo, juegos, comida, cupones, descuento, fotodepilación, joyería, oferta, promoción, promo, combocine, 2x1, almuerzo, desayuno, ropa de moda, trajes de boda, florería, fotografías, weeding planner, despedidas, locales, recepción, paquetes de viaje, Nuevo Mundo Viajes, excursiones, hoteles, casa playa, vestidos, accesorios para bebes, anillos, zapatos, ternos, buques, casa ideas, artículos, juguetes, dulcería, lentes, celulares, coche de bebe, libros, juegos mecánicos, cosméticos, maquillaje, supermercado, ropa para mujer, sandalias, juegos, jeans, billeteras, bolsos, perfumes, casacas, joyas, pareja,depa,dormitorios, departamento,inmobiliaria,San Isidro, San Borja,comunes,coworking,Surco,Surquillo"> 
+    
+ 
+    <!-- Facebook Pixel Code -->
+    <script>
+    window.onload = function(){
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2347629658600209'); 
+        fbq('track', 'PageView');
+    }
+    </script>
+
+    <script type='text/javascript'>
+        window.onload = function(){
+            var googletag = googletag || {};
+            googletag.cmd = googletag.cmd || [];
+            (function() {
+                var gads = document.createElement('script');
+                gads.async = true;
+                gads.type = 'text/javascript';
+                var useSSL = 'https:' == document.location.protocol;
+                gads.src = (useSSL ? 'https:' : 'http:') +
+                '//www.googletagservices.com/tag/js/gpt.js';
+                var node = document.getElementsByTagName('script')[0];
+                node.parentNode.insertBefore(gads, node);
+            })();
+        }
+    </script>
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127421070-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-127421070-1');
+    </script>
+
+
     <?php wp_head() ?> 
 
  
 </head>
-<body <?php body_class()  ?>  > 
+<body <?php body_class();  ?>  > 
