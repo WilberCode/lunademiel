f1cdec07101f8cd5d785bddc00f2161fad50e499
wilber <parionahuaraca@gmail.com>
2023-05-31 22:38:12 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -35,29 +35,39 @@
     }
     </script>
 
-    <script type='text/javascript'>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8DC95B28BG"></script>
+    <script>
         window.onload = function(){
-        setTimeout(function(){
-                var googletag = googletag || {};
-                googletag.cmd = googletag.cmd || [];
-                (function() {
-                    var gads = document.createElement('script');
-                    gads.async = true;
-                    gads.type = 'text/javascript';
-                    var useSSL = 'https:' == document.location.protocol;
-                    gads.src = (useSSL ? 'https:' : 'http:') +
-                    '//www.googletagservices.com/tag/js/gpt.js';
-                    var node = document.getElementsByTagName('script')[0];
-                    node.parentNode.insertBefore(gads, node);
-                })();
-            },3000)
+            setTimeout(function(){
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-8DC95B28BG');
+        },3000);
         }
     </script>
-
-
-   
-    <?php wp_head() ?> 
+ 
+   <!-- Google Tag Manager -->
+    <script>
+      window.onload = function(){
+        setTimeout(function(){
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5BVRXLK');
+            },3000);
+        }
+    </script>
+    <!-- End Google Tag Manager -->
+   <?php wp_head() ?> 
 
  
 </head>
 <body <?php body_class();  ?>  > 
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BVRXLK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
