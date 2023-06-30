20dbae3f299f760b85abae67117deea320299bc8
wilber <parionahuaraca@gmail.com>
2023-06-30 13:13:40 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -68,5 +68,5 @@
 <body <?php body_class();  ?>  > 
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BVRXLK"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
-    <!-- End Google Tag Manager (noscript)
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> 
+    <!-- End Google Tag Manager (noscript)-->
