027db579cdccac815e0b82e10891ee2c69757ddd
wilber <parionahuaraca@gmail.com>
2023-06-29 23:10:55 -0500

    cambios

--- page-casa.php
+++ page-casa.php
@@ -35,7 +35,19 @@ Template Post Type: post, page, event
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })(); 
     </script>
-
+   <!-- Google Tag Manager -->
+   <script>
+      window.onload = function(){
+        setTimeout(function(){
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5BVRXLK');
+            },3000);
+        }
+    </script>
+    <!-- End Google Tag Manager -->
  <style>
     *{
         padding: 0;
