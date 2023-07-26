55e54ddf4b2f5d51e832e63481da73f6441f024e
wilber <parionahuaraca@gmail.com>
2023-07-25 22:58:24 -0500

    cambios

--- page-d1.php
+++ page-d1.php
@@ -11,7 +11,7 @@ Template Post Type: post, page, event
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>D1 - Luna de miel</title>
+    <title>Landing D1 - Luna de miel</title>
     <meta name="description" content="D1 Movamos cosas ¿Bailamos?">
      
 
