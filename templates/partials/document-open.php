92fa1b6d5ea7ef4f069c44cd8ddaa61df6aa6d70
wilber <parionahuaraca@gmail.com>
2023-11-24 00:15:38 -0500

    camnbios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -62,7 +62,85 @@
         }
     </script>
     <!-- End Google Tag Manager -->
+ <style>
+    .single-content .entry img{
+	display:block;
+	margin-top:1rem;
+	margin-bottom:1rem;
+}
 
+.entry img[src*="/images/201601/"], .entry img[src*="/images/2013/"], .entry img[src*="/images/articulos/"],.entry img[src*="/images/"]{
+	display:none !important;
+}
+
+#mailpoet_form_2 .mailpoet_validate_success{
+	color:white !important;
+}
+
+.mailpoet_form_successfully_send .mailpoet_form_column,.mailpoet_form_successfully_send  .mailpoet_form_columns {
+	
+    flex-direction: row;
+}
+
+.mailpoet_message *{
+  color:white;	
+}
+
+ 
+
+.banner-home-content{
+	position:initial;
+	margin-top:0;
+	padding-top: 10px;
+	display:none;
+}
+.banner-home img{
+	width: 100%;
+}
+
+.titulo-editorial{
+	max-width: 523px !important;
+}
+
+@media (min-width:700px){ 
+	.float-left{
+		float:left;
+		margin-right:18px;
+	} 
+}
+.clear{clear:both;}
+
+
+/*banner*/
+.rslides_nav{
+	background: var(--primary) !important;
+	color:white;
+	display:none;
+	font-size:0;
+}
+.rslides_nav:before {
+	content:'>';
+	font-size: 20px;
+	
+}
+
+.rslides_nav:nth-child(2):before {
+	content:'<';
+	font-size: 20px; 
+}
+
+
+.banner-home:hover .rslides_nav{
+	 display:block;
+	transform: scaleY(1.8);
+}
+.metaslider  .rslides {
+	 margin:0 !important; 
+}
+.home-banner ul li img{
+	width: 100%;
+}
+ </style>
    
 </head>
 <body <?php body_class();  ?>  > 
