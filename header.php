21f670c69a931024b87a204e6f1668f15d1f2e4d
wilber <parionahuaraca@gmail.com>
2023-12-07 22:34:44 -0500

    cambios

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
  
-<?php if (  is_shop() || is_singular('product') || is_cart() || is_checkout() || is_account_page() ) { 
+<?php 
+/* 
+if (  is_shop() || is_singular('product') || is_cart() || is_checkout() || is_account_page() ) { 
     get_template_part( 'templates/partials/document-open-tienda' ); 
     
-} else{
-     get_template_part( 'templates/partials/document-open' );
- }
+} else{ */
+get_template_part( 'templates/partials/document-open' );
+/*  } */
     ?>
 <!-- Site header markup goes here -->  
 <?php if(is_front_page()){ ?>
