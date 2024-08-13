28f592248fb8b061e8b521d6f71a420c49441fe4
wilber <parionahuaraca@gmail.com>
2024-08-12 20:08:24 -0500

    cambiosd

--- front-page.php
+++ front-page.php
@@ -165,23 +165,23 @@ get_header();?>
                     <div class="flex justify-center" > 
                         <!-- Home - as post 1 -->
                         <ins class="adsbygoogle"
-                            style="display:inline-block;width:450px;heigh:272px"
+                            style="display:inline-block;width:953px;height:272px"
                             data-ad-client="ca-pub-2072313038095874"
                             data-ad-slot="5629535661"></ins>
                         <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
                     </div>
-                    <div class="flex justify-center" > 
+                    <!-- <div class="flex justify-center" >  -->
                         <!-- Home - as post 2 -->
-                        <ins class="adsbygoogle"
+                      <!--   <ins class="adsbygoogle"
                             style="display:inline-block;width:450px;height:272px"
                             data-ad-client="ca-pub-2072313038095874"
                             data-ad-slot="6256221624"></ins>
                         <script>
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
-                    </div> 
+                    </div>  -->
                    <!--  <div class=" hidden xl1:flex  " >  -->
                        <!-- Home - as post 3 -->
                      <!--    <ins class="adsbygoogle"
